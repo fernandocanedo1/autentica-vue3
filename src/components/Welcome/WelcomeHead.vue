08a7d9e6d617da6840f2ac83e8ad47aba6fd1857
fernandocanedo1 <fern.luck@gmail.com>
2023-04-17 10:33:10 -0300

    adicionado container 'Sair' na pagina de bem vindo

--- src/components/Welcome/WelcomeHead.vue
+++ src/components/Welcome/WelcomeHead.vue
@@ -1,55 +1,102 @@
 <template>
-    <section class="welcome-logo">
-        <img alt="Locaweb logo" src="../../assets/locaweb1.png" />
-    </section>
-    <section class="welcome-account">
-        <button class="account-button" type="button">{{ data.charAt(0) }}</button>
-    </section>
+  <section class="welcome-logo">
+    <img alt="Locaweb logo" src="../../assets/locaweb1.png" />
+  </section>
+  <div class="welcome-account" >
+      <button class="account-button" type="button" @click="toggleContainer">
+        {{ data.charAt(0) }}
+      </button>
+  </div>
+  <section class="welcome-container" v-show="showContainer">
+    <div class="welcome-container-bottom flex">
+      <a href="/">Sair</a>
+    </div>
+  </section>
 </template>
 
 <script >
 import { useStore } from "vuex";
 import { computed } from "vue";
 export default {
-    setup() {
-        const store = useStore();
+  setup() {
+    const store = useStore();
 
-        const data = computed(() => {
-            return store.state.name;
-        });
+    const data = computed(() => {
+      return store.state.name;
+    });
 
-        return { data };
-    },
-    props: {
-        user: String
+    return { data };
+  },
+data() {
+    return {
+      showContainer: false
+    };
+  },
+  props: {
+    user: String,
+  },
+  methods:{
+    toggleContainer() {
+      this.showContainer = !this.showContainer;
     }
-}
+  }
+};
 </script>
 
 <style>
 .welcome-logo {
-    position: absolute;
-    width: 176px;
-    height: 67px;
-    left: 27px;
-    top: 48px;
+  position: absolute;
+  width: 176px;
+  height: 67px;
+  left: 27px;
+  top: 48px;
 }
+.welcome-container {
+  position: absolute;
+  right: 0;
+  width: 71px;
+  height: 111px;
+  margin-top: 20px;
+  margin-right: 39px;
 
+  background: #FFFFFF;
+  border-radius: 5px;
+}
+.welcome-container-bottom {
+  position: absolute;
+  right: 0;
+  width: 131px;
+  height: 44px;
+  margin-top: 67px;
+  background: #FFFFFF;
+  border-radius: 5px;
+  justify-content: right;
+  align-items: center;
+}
+.welcome-container-bottom a {
+  padding-right: 20px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  text-decoration: none;
+}
 .welcome-account {
-    position: absolute;
-    right: 0;
-    padding-top: 27px;
-    padding-right: 49px;
+  position: absolute;
+  margin-right: 49px;
+  padding-top: 27px;
+  right: 0;
+  z-index: 100;
 }
 
 .account-button {
-    width: 52px;
-    height: 52px;
-    border-radius: 100px;
-    background: #D9D9D9;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 23px;
+  width: 52px;
+  height: 52px;
+  border-radius: 100px;
+  background: #d9d9d9;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 23px;
 }
 </style>
